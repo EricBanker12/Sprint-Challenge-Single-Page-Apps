5c395e7c50c849df98f81e6545d56a7f80ee791f
ericbanker12 <ericbanker12@gmail.com>
2019-09-20 11:16:55 -0500

    add search function

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+import SearchForm from "./SearchForm";
 import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
+  const [address, setAddress] = useState("https://rickandmortyapi.com/api/character/")
   const [characterList, setCharacterList] = useState([])
 
+  function search(str) {
+    setAddress(`https://rickandmortyapi.com/api/character/?name=${encodeURIComponent(str)}`)
+  }
+
   useEffect(() => {
-    axios.get("https://rickandmortyapi.com/api/character/")
+    axios.get(address)
       .then(resp => {
         setCharacterList(resp.data.results)
       })
@@ -15,11 +21,14 @@ export default function CharacterList() {
         console.error(err)
       })
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+  }, [address]);
 
   return (
-    <section className="character-list">
-      {characterList.map(char => <CharacterCard key={char.id} {...char} />)}
-    </section>
+    <>
+      <SearchForm search={search} />
+      <section className="character-list">
+        {characterList.map(char => <CharacterCard key={char.id} {...char} />)}
+      </section>
+    </>
   );
 }
