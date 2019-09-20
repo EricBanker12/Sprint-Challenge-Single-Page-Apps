710decfd20b497cd92d39c62ae495c315acf6f48
ericbanker12 <ericbanker12@gmail.com>
2019-09-20 11:44:29 -0500

    tweak styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,9 @@ export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/characters">Characters</NavLink>
+      <nav className="d-flex justify-content-center">
+        <NavLink exact to="/" className="mx-3" activeClassName="text-muted">Home</NavLink>
+        <NavLink to="/characters" className="mx-3" activeClassName="text-muted">Characters</NavLink>
       </nav>
     </header>
   )
