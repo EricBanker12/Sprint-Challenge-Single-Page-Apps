7a16654dfacf00e790ad12f99c2c8cf1c59dcc2d
ericbanker12 <ericbanker12@gmail.com>
2019-09-20 10:39:32 -0500

    add name to characterCard, links to header, route to characterList

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route } from "react-router-dom";
 
 import Header from "./components/Header.js";
 import WelcomePage from "./components/WelcomePage";
+import CharacterList from "./components/CharacterList.js";
 
 export default function App() {
   return (
     <main>
       <Header />
       <Route exact path="/" component={WelcomePage} />
+      <Route path="/characters" component={CharacterList} />
     </main>
   );
 }
