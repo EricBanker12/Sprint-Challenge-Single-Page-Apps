656fb7c55e6138097bc01b631b1ffd6751168b97
ericbanker12 <ericbanker12@gmail.com>
2019-09-20 11:03:17 -0500

    create SearchForm

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,25 @@
 import React, { useState } from "react";
 
-export default function SearchForm() {
- 
+export default function SearchForm(props) {
+  const [searchInput, setSearchInput] = useState('')
+
+  function handleInput(e) {
+    setSearchInput(e.target.value)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    props.search(searchInput)
+  }
+
   return (
     <section className="search-form">
-     // Add a search form here
+      <form onSubmit={handleSubmit}>
+        <label>
+          Search:&nbsp;
+          <input type="text" value={searchInput} onChange={handleInput} />
+        </label>
+      </form>
     </section>
   );
 }
